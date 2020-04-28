f61260a44a679a32ae08a03a4fac13f4d53b14ba
puderpach <64425089+puderpach@users.noreply.github.com>
2020-04-28 19:12:47 +0200

    First changes
    This is my first project, so "Hello World"!

---
+++ Hello World.js
@@ -0,0 +1,9 @@
+function changeColor() {
+    let text = document.getElementById("h1_change");
+    text.style.color = "blue";
+    text.style.fontFamily = "Comic Sans MS";
+    text.style.backgroundColor = "yellow";
+    text.style.borderWidth = "1px";
+    text.style.borderStyle = "solid";
+    text.style.borderColor = "black";
+}
